06d184fd12fcf08ada1f0b628c7e3c8f8a588f62
Михаил <pugumo68@gmail.com>
2019-06-26 23:14:37 +0700

    Added maket

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-        <h1>This is Russia</h1>
+        <user-card :full-name="'Mikhail Uljakov'" v-for="i in list"> </user-card>
     </div>
 </template>
 
 <script>
+    import UserCard from "../components/UserCard"
 
   export default {
-    name: "SecondPage"
+      name: "SecondPage",
+      components: {
+          UserCard
+      },
+      data() {
+          return {
+              list: [1,2,3,4,5,6,7,8]
+          }
+      }
   }
 </script>
 
