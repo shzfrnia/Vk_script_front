762e2fe2932282ec852dbe85d6a359b17c615864
Михаил <pugumo68@gmail.com>
2019-06-26 23:14:11 +0700

    Added animation of transition components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div :style="{'background-color': selectedBgColor}" id="app">
       <nav-bar></nav-bar>
-        <router-view/>
+      <div class="container">
+          <transition name="component-fade" mode="out-in">
+            <router-view/>
+          </transition>
+      </div>
   </div>
 </template>
 
@@ -28,5 +32,14 @@
         text-align: center;
         transition: 0.4s;
         height: 100vh;
+        overflow: auto;
+    }
+
+    .component-fade-enter-active, .component-fade-leave-active {
+        transition: opacity .15s ease;
+    }
+    .component-fade-enter, .component-fade-leave-to
+        /* .component-fade-leave-active до версии 2.1.8 */ {
+        opacity: 0;
     }
 </style>
