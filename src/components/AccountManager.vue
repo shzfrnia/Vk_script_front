ab205bde253c7f81f0b2354efac030a6722b01b2
Михаил <pugumo68@gmail.com>
2019-06-27 01:17:24 +0700

    YA HZ ESHE CHTO ETO

---
+++ src/components/AccountManager.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Это окошко с установленным аккаунтом</h1>
+</template>
+
+<script>
+    export default {
+        name: "AccountManager"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -3,11 +3,30 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+export const ACCOUNT_IS_SET = 0
+export const ACCOUNT_IS_NOT_SET = 1
+export const ACCOUNTIS_WATTING = 2
+
+
+
 export default new Vuex.Store({
   state: {
-
+    session: {
+      userIds: localStorage.getItem('userIds') || ''
+    },
+    friendList: [],
+    friendStates: {
+      banned: "banned",
+      active: null,
+      deleted: "deleted",
+      offline: true
+    },
+    errors: {
+      setAccount: {}
+    }
   },
   mutations: {
+
 
   },
   actions: {
