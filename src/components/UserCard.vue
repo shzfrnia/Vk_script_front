d583b9cfc800eea2ffb1f33e2335b34bba81021d
Unknown <pugumo68@gmail.com>
2019-07-13 17:24:55 +0700

    Delete src from img

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="media">
-        <img height="50" width="50" src="../../public/IMG_6639.jpg" class="mr-3 image-around" alt="...">
+        <img height="50" width="50" class="mr-3 image-around" alt="...">
         <div class="media-body">
             <h5 class="mt-0 text-left">{{fullName}}</h5>
         </div>
