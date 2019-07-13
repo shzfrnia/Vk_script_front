3534d5fffdeb0ccc754abe01fa7815a0860e238a
Unknown <pugumo68@gmail.com>
2019-07-13 15:17:41 +0700

    Add setting account

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {UserAPI} from "api-client";
 
 Vue.use(Vuex)
 
@@ -22,14 +23,27 @@ export default new Vuex.Store({
       offline: true
     },
     errors: {
-      setAccount: {}
+      setAccount: ""
     }
   },
   mutations: {
-
-
+    setAccount(state, user_ids) {
+      state.session.userIds = user_ids
+    },
+  getters: {
+    accountIsSet(state) {
+      return !!state.session.userIds || state.session.userIds !== ''
+    }
   },
   actions: {
-
+    async setAccount({commit}, user_ids) {
+      try {
+        const account = await UserAPI.getUser(user_ids)
+        commit('setAccount', account.id)
+      } catch (e) {
+        commit('setError', {form:'setAccount', errors: e.error})
+        commit('setAccount','')
+      }
+    }
   }
 })
