402cdc396bf7c6505718258cc640c1c342799f8a
Михаил <pugumo68@gmail.com>
2019-07-14 00:46:13 +0700

    Add all api

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 const API_ENDPINT = 'http://localhost:5000/api'
 
 const API_ROUTES = {
-  'user': `${API_ENDPINT}/user`
+  'user': `${API_ENDPINT}/user`,
+  'banned': `${API_ENDPINT}/user/banned_friends`,
+  'deleted': `${API_ENDPINT}/user/deleted_friends`,
+  'abandoned': `${API_ENDPINT}/user/abandoned_friends`
 }
 
 
@@ -13,16 +16,34 @@ export class UserAPI {
       const result = await axios.get(API_ROUTES.user,{params: {user_ids: user_ids}})
       return result.data
     } catch (e) {
-      throw {error: e}
+      throw {error: "User was not found"}
     }
   }
 
   static async getBannedFriends(user_ids) {
+    try {
+      const result = await axios.get(API_ROUTES.banned,{params: {user_ids: user_ids}})
+      return result.data
+    } catch (e) {
+      throw {error: e}
+    }
   }
 
   static async getDeletedFriends(user_ids) {
+    try {
+      const result = await axios.get(API_ROUTES.deleted,{params: {user_ids: user_ids}})
+      return result.data
+    } catch (e) {
+      throw {error: e}
+    }
   }
 
   static async getAbandonedFriends(user_ids) {
+    try {
+      const result = await axios.get(API_ROUTES.abandoned,{params: {user_ids: user_ids}})
+      return result.data
+    } catch (e) {
+      throw {error: e}
+    }
   }
 }
