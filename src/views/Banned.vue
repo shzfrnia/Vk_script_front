b0f73da700c093d5dbe21cece0a6f14b3d8e6247
Михаил <pugumo68@gmail.com>
2019-07-14 01:50:20 +0700

    Add openning acc on click

--- src/views/Banned.vue
+++ src/views/Banned.vue
@@ -1,26 +1,9 @@
 <template>
     <div style="width: 100%">
-        <md-card md-with-hover>
-            <!--   HERE ANIMATION         -->
-            <md-ripple>
-            <!--                -->
-            <md-card-header>
-                <md-card-media>
-                    <img src="https://vuematerial.io/assets/examples/avatar-2.jpg" alt="People">
-                </md-card-media>
-                <md-card-header-text>
-                    <div class="md-title">Media card</div>
-<!--                    <div class="md-subhead">Normal size</div>-->
-                </md-card-header-text>
-
-            </md-card-header>
-<!--            <md-card-actions>-->
-<!--                <md-button>Action</md-button>-->
-<!--                <md-button>Action</md-button>-->
-<!--            </md-card-actions>-->
-            </md-ripple>
-        </md-card>
-<!--        <user-card :key="friend.id" :account="friend" v-for="friend in bannedFriends"> </user-card>-->
+        <user-card @click.native="clickOnUser(index)"
+                   :key="friend.id"
+                   :account="friend"
+                   v-for="friend in bannedFriends"> </user-card>
     </div>
 </template>
 
@@ -36,15 +19,18 @@
       bannedFriends() {
         return this.$store.state.bannedFriends
       }
+    },
+    methods: {
+      clickOnUser(index) {
+        this.openInNewTab(this.$store.state.abandonedFriends[index].link)
+      },
+      openInNewTab(url) {
+        const win = window.open(url, '_blank');
+        win.focus();
+      }
     }
   }
 </script>
 
 <style scoped>
-    .md-card {
-        background: white;
-        min-height: 100px;
-        height: 110px;
-    }
-
 </style>
