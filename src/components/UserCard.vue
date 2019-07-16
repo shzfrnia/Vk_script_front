c0ce22f22c832e708b3869af525b2701dca2cd9a
Unknown <pugumo68@gmail.com>
2019-07-17 02:34:58 +0700

    add meta param

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,37 +1,37 @@
 <template>
     <md-card md-with-hover>
-        <!--   HERE ANIMATION         -->
         <md-ripple>
-            <!--                -->
             <md-card-header>
                 <md-card-media>
                     <img :src="account.avatar" alt="People">
                 </md-card-media>
                 <md-card-header-text>
                     <div class="md-title">{{account.first_name}} {{account.last_name}}</div>
-                    <!--                    <div class="md-subhead">Normal size</div>-->
+                    <div v-if="showMeta" class="md-subhead">Days offline {{account.days_offline}}</div>
                 </md-card-header-text>
-
             </md-card-header>
-            <!--            <md-card-actions>-->
-            <!--                <md-button>Action</md-button>-->
-            <!--                <md-button>Action</md-button>-->
-            <!--            </md-card-actions>-->
         </md-ripple>
     </md-card>
 </template>
 
 <script>
-
     export default {
       name: "UserCard",
       props: {
-        account: Object
+        account: Object,
+        showMeta: {
+          type: Boolean,
+          default: false
+        }
       }
     }
 </script>
 
 <style scoped>
+    .md-card-header-text {
+        padding: 0 10px 0 10px;
+    }
+
     .md-card {
         background: white;
         height: 110px;
