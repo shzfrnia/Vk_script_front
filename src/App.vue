1d8c5fa2b1d4046de9d6e3e5051ab366abf691e7
Unknown <pugumo68@gmail.com>
2019-07-17 02:34:42 +0700

    Add material icon and rework app's style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div :style="{'background-color': selectedBgColor}" id="app">
       <nav-bar></nav-bar>
-      <div style="display: flex; height: 100%;" class="container">
+      <div class="md-layout">
           <transition name="component-fade" mode="out-in">
             <router-view/>
           </transition>
@@ -25,16 +25,14 @@
 
 <style>
     @import 'assets/fonts/fontAwesome.css';
+    @import 'assets/fonts/materialIcons.css';
 
     #app {
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
         transition: 0.4s;
         height: 100vh;
         overflow: auto;
-        display: flex;
-        flex-direction: column;
     }
 
     .component-fade-enter-active, .component-fade-leave-active {
