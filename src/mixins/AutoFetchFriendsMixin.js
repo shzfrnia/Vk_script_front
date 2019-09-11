52b6124304a7d80dc60c80b9b530667db00d7606
Unknown <pugumo68@gmail.com>
2019-09-11 19:23:00 +0700

    Add check out url's query

--- src/mixins/AutoFetchFriendsMixin.js
+++ src/mixins/AutoFetchFriendsMixin.js
@@ -3,6 +3,15 @@ export default {
     if (this.$store.state.fetched) {
       return
     }
+    const link = this.$route.query.link
+    if (link) {
+      try {
+        await this.$store.dispatch('setAccount', link)
+      } catch (e) {
+        this.$router.push({name: 'home'})
+      }
+      await this.$store.dispatch('fetchAllFriends', this.$store.state.session.userIds)
+    }
     if (this.$store.getters.accountIsSet) {
       await this.$store.dispatch('fetchAllFriends', this.$store.state.session.userIds)
     } else {
