368b8833ec6b3400bb9d8b63e344b53995b18973
Mikhail <pugumo68@gmail.com>
2020-03-31 01:57:52 +0700

    Add pluralization for days offline message and translate it

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,39 +1,42 @@
 <template>
-    <md-card md-with-hover>
-        <md-ripple>
-            <md-card-header>
-                <md-card-media>
-                    <img :src="account.avatar" alt="People">
-                </md-card-media>
-                <md-card-header-text>
-                    <div class="md-title">{{account.first_name}} {{account.last_name}}</div>
-                    <div v-if="showMeta" class="md-subhead">Days offline {{account.days_offline}}</div>
-                </md-card-header-text>
-            </md-card-header>
-        </md-ripple>
-    </md-card>
+  <md-card md-with-hover>
+    <md-ripple>
+      <md-card-header>
+        <md-card-media>
+          <img :src="account.avatar" alt="People" />
+        </md-card-media>
+        <md-card-header-text>
+          <div class="md-title">{{account.first_name}} {{account.last_name}}</div>
+          <div
+            v-if="showMeta"
+            class="md-subhead"
+          >{{$tc('abandoned.daysOffline', account.days_offline, {n: account.days_offline})}}</div>
+        </md-card-header-text>
+      </md-card-header>
+    </md-ripple>
+  </md-card>
 </template>
 
 <script>
-    export default {
-      name: "UserCard",
-      props: {
-        account: Object,
-        showMeta: {
-          type: Boolean,
-          default: false
-        }
-      }
+export default {
+  name: "UserCard",
+  props: {
+    account: Object,
+    showMeta: {
+      type: Boolean,
+      default: false
     }
+  }
+};
 </script>
 
 <style scoped>
-    .md-card-header-text {
-        padding: 0 10px 0 10px;
-    }
+.md-card-header-text {
+  padding: 0 10px 0 10px;
+}
 
-    .md-card {
-        background: white;
-        height: 110px;
-    }
+.md-card {
+  background: white;
+  height: 110px;
+}
 </style>
