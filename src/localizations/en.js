f3aeff38f3d41121d53665b0fcc7d50e9918b27c
Mikhail <pugumo68@gmail.com>
2020-03-31 01:58:14 +0700

    Add translations

--- src/localizations/en.js
+++ src/localizations/en.js
@@ -3,5 +3,19 @@ export default {
         banned: "Banned",
         deleted: "Deleted",
         abandoned: "Abandoned"
+    },
+    home: {
+        linkLabel: "Account's link",
+        scanAccount: "Scan account"
+    },
+    abandoned: {
+        filters: {
+            descending: "descending",
+            ascending: "ascending",
+            name: "name"
+        },
+        sortBy: "Sort by",
+        daysOfflineLabel: "Days offline",
+        daysOffline: "Zero days offline | One day offline| {n} days offline"
     }
 }
--- src/localizations/rus.js
+++ src/localizations/rus.js
@@ -3,5 +3,19 @@ export default {
         banned: "Забанненые",
         deleted: "Удалившиеся",
         abandoned: "Забросившие"
+    },
+    home: {
+        linkLabel: "Ссылка на аккаунт",
+        scanAccount: "Сканировать аккаунт"
+    },
+    abandoned: {
+        filters: {
+            descending: "по убвыванию",
+            ascending: "по возрастанию",
+            name: "по имени"
+        },
+        sortBy: "Сортировать по",
+        daysOfflineLabel: "Дней оффлайн",
+        daysOffline: "Ноль дней офлайн | Один день оффлайн | {n} дней оффлайн"
     }
 }
