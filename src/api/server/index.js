1711ecdd9743fd1e3daeb7b75a657a9f205fe8a7
Mikhail Uljakov <pugumo68@gmail.com>
2021-02-26 02:35:57 +0700

    Fix routes.

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
-const API_ENDPOINT = '/api'
+const API_ENDPOINT = `${process.env.VUE_APP_API_HOST}/api`
 
 const API_ROUTES = {
-  'user': `${API_ENDPOINT}/user`,
-  'banned': `${API_ENDPOINT}/user/banned_friends`,
-  'deleted': `${API_ENDPOINT}/user/deleted_friends`,
-  'abandoned': `${API_ENDPOINT}/user/abandoned_friends`,
-  'friendsList': `${API_ENDPOINT}/user/friends`
+  'user': `${API_ENDPOINT}/user/`,
+  'banned': `${API_ENDPOINT}/user/banned_friends/`,
+  'deleted': `${API_ENDPOINT}/user/deleted_friends/`,
+  'abandoned': `${API_ENDPOINT}/user/abandoned_friends/`,
+  'friendsList': `${API_ENDPOINT}/user/friends/`
 }
 
 
 export class UserAPI {
   static async getUser(user_ids) {
     try {
-      const result = await axios.get(API_ROUTES.user,{params: {user_ids: user_ids}})
+      const result = await axios.get(API_ROUTES.user + user_ids)
       return result.data
     } catch (e) {
       throw {error: "Аккаунт не найден"}
@@ -23,7 +23,7 @@ export class UserAPI {
 
   static async getBannedFriends(user_ids) {
     try {
-      const result = await axios.get(API_ROUTES.banned,{params: {user_ids: user_ids}})
+      const result = await axios.get(API_ROUTES.banned + user_ids)
       return result.data
     } catch (e) {
       throw {error: e}
@@ -32,7 +32,7 @@ export class UserAPI {
 
   static async getDeletedFriends(user_ids) {
     try {
-      const result = await axios.get(API_ROUTES.deleted,{params: {user_ids: user_ids}})
+      const result = await axios.get(API_ROUTES.deleted + user_ids)
       return result.data
     } catch (e) {
       throw {error: e}
@@ -41,7 +41,7 @@ export class UserAPI {
 
   static async getAbandonedFriends(user_ids) {
     try {
-      const result = await axios.get(API_ROUTES.abandoned,{params: {user_ids: user_ids, days_offline: 999}})
+      const result = await axios.get(API_ROUTES.abandoned + user_ids,{params: {days_offline: 999}})
       return result.data
     } catch (e) {
       throw {error: e}
@@ -50,7 +50,7 @@ export class UserAPI {
 
   static async getFriendsList(user_ids) {
     try {
-      const result = await axios.get(API_ROUTES.friendsList,{params: {user_ids: user_ids}})
+      const result = await axios.get(API_ROUTES.friendsList + user_ids)
       return result.data
     } catch (e) {
       throw {error: e}
